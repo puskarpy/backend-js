cfe17cebaff2771d78a59eac545f59ad14fe1185
puskarpy <puskarniraula11@gmail.com>
2025-11-20 19:14:05 +0545

    User and Video model updated.

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+    constructor(statusCode, message = "success", data){
+        this.statusCode = statusCode
+        this.data = data
+        this.success = statusCode < 400
+        this.message = message
+    }
+
+}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+export const asyncHandler = (fn) => {
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+
+// export const asyncHandler = (fn) => {
+//     async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         return res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+//     }
+// }
