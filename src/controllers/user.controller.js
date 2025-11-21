55fb50189b2b02fea62b2de294e01e0db94df882
puskarpy <puskarniraula11@gmail.com>
2025-11-21 15:55:25 +0545

    User controller added.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,62 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 
 export const registerUser = asyncHandler( async(req, res) => {
-    res.status(200).json({
-        message: "Ok"
+    // Getting data from body
+    const { fullName, email, username, password, avatar } = req.body
+
+    // Data Validaition
+    if(!fullName || !email || username|| !password || !avatar){
+        throw new ApiError(400, "All Fields required.")
+    }
+
+    // Checking if user already exists
+    const existingUser = User.findOne({
+        $or: [{username}, {email}]
     })
+    if(existingUser){
+        throw new ApiError(409, "User already exists.")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required.")
+    }
+
+    // Upload to cloudinary
+   const avatarImage = await uploadOnCloudinary(avatarLocalPath)
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   if(!avatarImage) {
+    throw new ApiError(400, "API is required")
+   }
+
+   // Create user
+   const user = await User.create({
+    fullName,
+    email,
+    password,
+    username: username.toLowerCase(),
+    avatar: avatarImage.url,
+    coverImage: coverImage?.url || ""
+   })
+
+   const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+   )
+
+   if(!createdUser){
+    throw new ApiError(500, "Internal Server user.")
+   }
+
+   return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully")
+   )
+
 } )
--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -1,9 +1,8 @@
-class ApiResponse {
-    constructor(statusCode, message = "success", data){
+export class ApiResponse {
+    constructor(statusCode, data, message = "success"){
         this.statusCode = statusCode
         this.data = data
         this.success = statusCode < 400
         this.message = message
     }
-
 }
